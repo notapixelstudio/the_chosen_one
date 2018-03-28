29b6f3fef73390aa9c3962760e65804899deefa2
salvob <tholalak@yahoo.it>
2018-03-28 03:36:30 +0200

    dinamically created button

--- CharacterCreationRoom.gd
+++ CharacterCreationRoom.gd
@@ -10,15 +10,34 @@ var hair_i = 0
 func _ready():
 	# FIXME this should go to the game initialization script
 	randomize()
-	
 	Character = load('res://character/Character.gd').new().Character
 	character = Character.new()
 	$CharacterPreview.load_character(character)
-	
+	var feats = character.FEATURES
+	var nodo_capelli = get_node("hair")
 
-func on_pressed(button):
-	print(button.get_name())
-	character.features.hair = hair_types[hair_i]
-	hair_i = (hair_i+1) % len(hair_types)
+	var i = 0
+	var j = 0
+	for key in feats:
+		var n = get_node(key)
+		n.name = key
+		j = 0
+		for value in feats[key]:
+			var button = Button.new()
+			button.set_theme(load("res://screens/theme.tres"))
+			# TODO: MAtteo guarda queste posizioni
+			button.set_begin(Vector2(10,j+90))
+			button.set_end(Vector2(90,j+90))
+			button.rect_position = Vector2(j+i+90,i+j+100)
+			button.name = value
+			button.text = value.replace("_", " ")
+			n.add_child(button)
+			button.add_to_group(key)
+			button.connect("pressed", self, "on_feature_selected", [key,value])
+			j+=1
+	
+func on_feature_selected(feature, value):
+	character.features[feature] = value
 	$CharacterPreview.update_preview()
-		
+	pass
+	
