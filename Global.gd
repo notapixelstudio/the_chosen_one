b4ea370d4f2eba8724d347fc2af6596b9c9bd78c
nitaku <nitaku@gmail.com>
2018-03-30 12:33:13 +0200

    Debug feedback: who is the chosen one?

--- Global.gd
+++ Global.gd
@@ -17,6 +17,10 @@ func _ready():
 
 	chosen_one_data = CharacterData.new()
 	
+	# debug feedback
+	print('The Chosen One is:')
+	print(chosen_one_data.features)
+	
 	# this is to avoid starting the game with the chosen one by chanche
 	# at least WRONG_FEATURES features have to be wrong
 	while player_character_data == null or player_character_data.diff(chosen_one_data).size() < WRONG_FEATURES:
