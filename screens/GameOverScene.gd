436c6e1a94f9cef055b22a8f0c0152021c688773
salvob <tholalak@yahoo.it>
2018-04-03 15:49:18 +0200

    first try for a progress bar

--- screens/GameOverScene.gd
+++ screens/GameOverScene.gd
@@ -3,6 +3,8 @@ extends "res://screens/basic_screen.gd"
 func _ready():
 	$Animator.play("YOUDIED")
 	$Button.connect("button_up", self, "on_try_again")
+	$Animator.play("loading")
 	
 func on_try_again():
+
 	change_scene()
