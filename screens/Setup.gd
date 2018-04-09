3ef83fda0d383e7900cdac0ea1f7a631b8f463c2
salvob <tholalak@yahoo.it>
2018-04-09 12:28:42 +0200

    added transition to Character Room. Speed up change scene transition

--- screens/Setup.gd
+++ screens/Setup.gd
@@ -5,3 +5,8 @@ signal progress_finished
 func _on_Setup_transition_finished():
 	print("animation progress finished")
 	change_scene()
+
+func change_scene(to = next_scene):
+	# speed up the process. 
+	# TODO: Needs some understanding 
+	get_tree().change_scene(to)
